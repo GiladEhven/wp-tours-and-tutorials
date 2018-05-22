6a48002a5ab1f714e60149bc8f031e7dded28fc2
Gilad Ehven <gilad@ehven.com>
2018-05-22 17:09:08 -0400

    First localhost commit: Base file system (including untested [composer.json]); parallels 'wordpress-plugin-starter-type-four'.

---
+++ wp-tours-and-tutorials.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * WordPress plugin that adds interactive guides and tutorials to the WordPress Admin and certain public-side admin tools
+ *
+ * @author                  Gilad Ehven
+ * @copyright               2018 Gilad Ehven (Ehventerprise LLC)
+ * @license                 GNU General Public License 2.0+
+ * @link                    https://github.com/GiladEhven/wp-tours-and-tutorials
+ * @package                 WpToursAndTutorials
+ * @since                   0.1.0
+ *
+ * @wordpress-plugin
+ *
+ * Plugin Name:             WP Tours and Tutorials
+ *
+ * Author:                  Gilad Ehven
+ * Author URI:              http://gilad-ehven.com
+ * Contributors:            Mihal Ehven (Ehventerprise LLC)
+ * Description:             This plugin adds interactive guides and tutorials to the WordPress Admin and certain public-side admin tools.
+ * Domain Path:             /languages
+ * License:                 GNU General Public License 2.0+
+ * License URI:             http://www.gnu.org/licenses/gpl-2.0.txt
+ * Network:                 False
+ * Plugin URI:              https://github.com/GiladEhven/wp-tours-and-tutorials
+ * Privacy Policy:          https://gilad-ehven.com/privacy/
+ * Terms of Use:            https://gilad-ehven.com/terms/
+ * Text Domain:             wp-tours-and-tutorials
+ * Version:                 0.1.0
+ *
+ */
+
+namespace WpToursAndTutorials;
+
+if ( ! defined( 'ABSPATH' ) ) exit( "Nothing to see here. Sorry. Try the home page." );
